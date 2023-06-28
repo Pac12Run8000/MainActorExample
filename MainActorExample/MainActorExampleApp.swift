b064092872c37aafb4b76d2a6f843cbfce605151
Pac12Run8000 <gangstarr78992@>
2023-06-28 13:11:08 -0700

    Initial Commit

---
+++ MainActorExample/MainActorExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MainActorExampleApp.swift
+//  MainActorExample
+//
+//  Created by Michelle Grover on 6/28/23.
+//
+
+import SwiftUI
+
+@main
+struct MainActorExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

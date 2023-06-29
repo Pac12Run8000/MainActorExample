6b8677674ffedc681f41805e95a3d407c47fef5a
Pac12Run8000 <gangstarr78992@>
2023-06-28 18:51:05 -0700

    Added a new image to functionality

--- MainActorExample/Views/ContentView.swift
+++ MainActorExample/Views/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        AsyncImageView(url: URL(string: "https://cdn.asianmma.com/wp-content/uploads/2022/04/Khamzat-Chimaev-2.jpg")!)
+        AsyncImageView(url: URL(string: "https://cdn.bloodyelbow.com/wp-content/uploads/2023/06/mma-ufc-279-chimaev-vs-holland-1015249553-1-scaled.jpg?width=1280&aspect_ratio=16:10")!)
     }
 }
 
